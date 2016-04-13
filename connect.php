003b8f74b550107c4921c0ecf8e8f51951ca0721
wwmwingman <wingman518@hotmail.com>
2016-04-13 16:27:48 +0800

    connection

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+$hostname = "localhost";
+$username = "peakwongwingman";
+$password ="wongwingman";
+$connection = mysql_connect($hostname, $username, $password) or die("Could not open connection to database");
+?>
+
